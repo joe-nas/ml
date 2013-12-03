807fc54e3eb3ae1411e056cdf23119715828d838
joe-nas <jonas.falck@student.ru.nl>
2013-12-03 15:52:08 +0100

    unreg cost function submitted

--- pa4/mlclass-ex4/nnCostFunction.m
+++ pa4/mlclass-ex4/nnCostFunction.m
@@ -62,16 +62,17 @@ Theta2_grad = zeros(size(Theta2));
 %               and Theta2_grad from Part 2.
 %
 
-X = [ones(size(X,1),1),X];
 permmat = eye(num_labels)(y,:);
-z2 = X*Theta1';
+a1 = [ones(size(X,1),1),X];
+z2 = a1*Theta1';
 a2 = sigmoid(z2);
 a2 = [ones(size(a2,1),1),a2];
 z3 = a2*Theta2';
 a3 = sigmoid(z3);
 
-J = (-y.* log(a3) - (1-y).*log(1 - a3)) * permmat'
-size(J)
+J = 1/m * sum(sum(((-permmat .* log(a3) - (1-permmat) .* log(1 - a3)))'));
+J
+
 
 
 
